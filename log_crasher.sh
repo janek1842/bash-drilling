3af353a4e63313b03a9ce59fca53bdd81256ad92
Jan <56030577+janek1842@users.noreply.github.com>
2023-08-31 11:14:04 +0200

    extensionCounter extension :D

--- log_crasher.sh
+++ log_crasher.sh
@@ -7,11 +7,20 @@
 function pathGiver() {
     echo "PLEASE ENTER THE DIRECTORY NAME THAT CONTAINS THE ANALYZED DATA: "
     read CHOICE_DIR
-    echo "You have entered following path: " $CHOICE_DIR
+    echo "You have entered following directory: " $CHOICE_DIR
+
+    if test -d $CHOICE_DIR; then
+        echo "Directory exists."
+    else
+        echo "Wrong directory name, exiting ... "
+        echo ""
+        exit
+    fi
+
     echo ""
     echo "Please select the operation type: "
     echo ""
-    select y in  WordCount Quit
+    select y in  WordCount FileExtensionCount Quit
         do
         case $y in "WordCount") 
         echo "-----------------------" 
@@ -19,6 +28,12 @@ function pathGiver() {
         echo "Please select the word: "
         read word
         wordCounter $word $CHOICE_DIR;;
+        "FileExtensionCount") 
+        echo "-----------------------" 
+        echo ""
+        echo "Please select the extension (without dot): "
+        read extension
+        fileExtensionCounter $extension $CHOICE_DIR;;
         "Quit") exit ;;
         *) echo "Exiting ... "
         exit 1
@@ -36,10 +51,16 @@ function wordCounter() {
     find ./${a} -name "*.log" | xargs grep -oh $b | wc -w  
 }
 
-
+function fileExtensionCounter() {
+    echo " " 
+    echo "NUMBER OF FILES WITH EXTENSION:" \"$1\" "IN THE DIRECTORY:" \"$2\"
+    echo " " 
+    a=$2
+    b=$1
+    find ./${a} -name "*.${b}" | wc -w  
+}
 
 function main() {
-
     echo ""
     echo "______________________________LOG CRASHER_______________________________________"
     echo "      WELCOME IN LOG_CRASHER - BASH TOOL FOR MAKING YOUR LOG DATA MORE CLEAR    "
