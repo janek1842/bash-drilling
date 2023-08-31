19f3b48f7486a5dacd3f5fecd035e54091b89dc0
Jan <56030577+janek1842@users.noreply.github.com>
2023-08-31 17:07:20 +0200

    Update log_crasher.sh

--- log_crasher.sh
+++ log_crasher.sh
@@ -4,6 +4,14 @@
 # by sharing the most common operations used during log processing with awk, sed and grep options
 # Logs can be taken i.e. from https://github.com/logpai/loghub 
 
+# Functionalities delievered so far:
+# 1) WordCount                     
+# 2) FileExtensionCount            
+# 3) nMostCommonWordsCounter 
+# 4) nMostCommonExtensionsCounter
+# 5) nBiggestFilesCounter
+# 6) nLatestFilesCounter
+
 function pathGiver() {
     echo "PLEASE ENTER THE DIRECTORY NAME THAT CONTAINS THE ANALYZED DATA: "
     read CHOICE_DIR
@@ -20,7 +28,7 @@ function pathGiver() {
     echo ""
     echo "Please select the operation type: "
     echo ""
-    select y in  WordCount FileExtensionCount Quit
+    select y in  WordCount FileExtensionCount nMostCommonWordsCounter nMostCommonExtensionsCounter nBiggestFilesCounter nLatestFilesCounter Quit
         do
         case $y in "WordCount") 
         echo "-----------------------" 
@@ -34,6 +42,31 @@ function pathGiver() {
         echo "Please select the extension (without dot): "
         read extension
         fileExtensionCounter $extension $CHOICE_DIR;;
+        "nMostCommonWordsCounter") 
+        echo "-----------------------" 
+        echo ""
+        echo "Please select the number of most common words: "
+        read numbers
+        nMostCommonWordsCounter $numbers $CHOICE_DIR;;
+        "nMostCommonExtensionsCounter") 
+        echo "-----------------------" 
+        echo ""
+        echo "Please select the number of most common extensions: "
+        read numbers
+        nMostCommonExtensionsCounter $numbers $CHOICE_DIR;;
+        "nBiggestFilesCounter") 
+        echo "-----------------------" 
+        echo ""
+        echo "Please select the number of the biggest files: "
+        read numbers
+        nBiggestFilesCounter $numbers $CHOICE_DIR;;
+        "Quit") exit ;;
+        "nLatestFilesCounter") 
+        echo "-----------------------" 
+        echo ""
+        echo "Please select the number of the recently modified files: "
+        read numbers
+        nLatestFilesCounter $numbers $CHOICE_DIR;;
         "Quit") exit ;;
         *) echo "Exiting ... "
         exit 1
@@ -60,6 +93,51 @@ function fileExtensionCounter() {
     find ./${a} -name "*.${b}" | wc -w  
 }
 
+function fileExtensionCounter() {
+    echo " " 
+    echo "NUMBER OF FILES WITH EXTENSION:" \"$1\" "IN THE DIRECTORY:" \"$2\"
+    echo " " 
+    a=$2
+    b=$1
+    find ./${a} -name "*.${b}" | wc -w  
+}
+
+function nMostCommonWordsCounter() {
+    echo " " 
+    echo "LISTING" $1 "MOST COMMON WORDS IN THE DIRECTORY:" \"$2\"
+    echo " " 
+    a=$2
+    b=$1
+    find ./${a} -name "*.log" | xargs sed -e 's/\s/\n/g' | sort | uniq -c | sort -nr | head -${b}
+}
+
+function nMostCommonExtensionsCounter() {
+    echo " " 
+    echo "LISTING" $1 "MOST COMMON FILE EXTENSIONS IN THE DIRECTORY:" \"$2\"
+    echo " " 
+    a=$2
+    b=$1
+    find ./${a} -type f | sed 's/.*\.//' | sort | uniq -c | sort -nr | head -n ${b}
+}
+
+function nBiggestFilesCounter() {
+    echo " " 
+    echo "LISTING" $1 "BIGGEST FILES IN THE DIRECTORY:" \"$2\"
+    echo " " 
+    a=$2
+    b=$1
+    find ./${a} -printf '%s %p\n'| sort -nr | head -n ${b}
+}
+
+function nLatestFilesCounter() {
+    echo " " 
+    echo "LISTING" $1 "RECENTLY MODIFIED FILES IN THE DIRECTORY:" \"$2\"
+    echo " " 
+    a=$2
+    b=$1
+    find ./${a} -type f | xargs ls -lt | head -n ${b} | awk '{print $6,$7,$8,$9}'
+}
+
 function main() {
     echo ""
     echo "______________________________LOG CRASHER_______________________________________"
@@ -69,5 +147,4 @@ function main() {
     pathGiver
 }
 
-mainDir=""
 main
